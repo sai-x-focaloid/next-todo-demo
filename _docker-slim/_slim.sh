bca5609bf659ee0832cd5be2c39e5ff0bfa1339a
nipunshaji190 <71396422+nipunshaji190@users.noreply.github.com>
2021-01-08 22:58:16 +0530

    Update _slim.sh

--- _docker-slim/_slim.sh
+++ _docker-slim/_slim.sh
@@ -9,6 +9,10 @@ BDIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 pushd $BDIR
 source .env
 
+
+# Without the include-path flags, we found that some of the node modules were missing from the slimmed image
+# And this caused run time error in app
+
 docker-slim build --target node-server:test --env DB_URL=$DB_URL --include-path /app/node_modules
 docker-slim build --target next-client:test --include-path /app/node_modules
 
